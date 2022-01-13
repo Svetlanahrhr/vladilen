1407f70901c3bf48dda9e8582862a2d6c445b647
Svetlana <kovalenkohp@mail.ru>
2022-01-13 12:11:15 +0400

    init: end drag and drop

--- 02-drag-n-drop-sources/app.js
+++ 02-drag-n-drop-sources/app.js
@@ -6,14 +6,12 @@ item.addEventListener('dragend', dragend);
 
 for (placeholder of placeholders) {
 	placeholder.addEventListener('dragover', dragover);
-	placeholder.addEventListener('dragleave', dragleave);
 	placeholder.addEventListener('dragenter', dragenter);
+	placeholder.addEventListener('dragleave', dragleave);
 	placeholder.addEventListener('drop', dragDrop);
 }
 
 function dragstart (event) {
-	console.log(event.target);
-	
 	setTimeout(() => event.target.classList.add('hide'), 0);
 	event.target.classList.add('border');
 	
@@ -22,4 +20,22 @@ function dragstart (event) {
 function dragend (event) {
 	console.log(event.target);
 	event.target.className = 'item';
+}
+// перетаскиваемый элемент над
+function dragover (event) {
+	event.preventDefault();
+	event.target.classList.add('border_top');
+	
+}
+function dragenter (event) {
+	console.log('dragenter');
+}
+// перетаскиваемый элемент покидает место и 
+function dragleave (event) {
+	console.log('dragleave');
+	event.target.className = 'placeholder';
+}
+function dragDrop (event) {
+	event.target.classList.remove('border_top');
+	event.target.append(item); 
 }
